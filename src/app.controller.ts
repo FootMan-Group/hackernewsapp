bb222131895b58c7fd866da3617149b461246233
maxwell.iragu <maxwell.iragu@belmax.org>
2022-06-05 15:27:31 +0300

    1. Pulls data from Hacker news and makes a counter of the 10 most used words 2. Pulls data of the users with Karma over 10000 and takes their most recent counter and gets the 10 most used words

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,105 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { map, observable } from 'rxjs';
 
 @Controller()
 export class AppController {
+  private data = {
+    titlemost25: {
+      HNLINK: '',
+      titles: [],
+      stories: [],
+    },
+    titleMost23Week: {},
+    titlemost600: {},
+  };
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  sortCounter(mostTitles) {
+    let wordCounts = {};
+    let words = mostTitles.split(/\s/);
+    for (let i = 0; i < words.length; i++)
+      wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1;
+    let items = Object.keys(wordCounts).map((key) => {
+      return [key, wordCounts[key]];
+    });
+
+    items.sort((first, second) => {
+      return second[1] - first[1];
+    });
+
+    return items.slice(0, 10);
+  }
+
+  @Get(':param')
+  async getHello(@Param() params) {
+    let titles = 0;
+    let stories = 0;
+    let mostRecent = [];
+    let mostTitles = '';
+    let wordCounts = {};
+    let mostUsers = [];
+    let finalData = [];
+
+    if (params.param == 'titlemost25') {
+      params.HNLINK = 'newstories.json?print=pretty';
+      await this.appService.getData(params).then(function (this, results) {
+        while (titles <= 25) {
+          mostRecent.push(results[titles]);
+          titles++;
+        }
+      });
+
+      while (stories <= 25) {
+        params.HNLINK = 'item/' + mostRecent[stories] + '.json?print=pretty';
+        await this.appService.getData(params).then(function (this, results) {
+          mostTitles += results.title + ' ';
+        });
+
+        stories++;
+      }
+
+      mostTitles.toLowerCase();
+      mostTitles.replace(/[^0-9a-z ]/g, '');
+      const finalResults = this.sortCounter(mostTitles);
+
+      console.log(finalResults);
+      return finalResults;
+
+    }
+
+    else if (params.param == 'topusers') {
+      params.HNLINK = 'newstories.json?print=pretty';
+      await this.appService.getData(params).then(function (this, results) {
+        mostUsers.push(results);
+      });
+
+      for (let i = 0; i <= 25; i++) {
+        params.HNLINK = 'item/' + mostUsers[0][i] + '.json?print=pretty';
+        await this.appService.getData(params).then(function (this, results) {
+          mostUsers[0][i] = {
+            title: results.title,
+            byUser: results.by,
+            storyID: results.id,
+          };
+        });
+      }
+
+      for (let x = 0; x <= 25; x++) {
+        params.HNLINK = 'user/' + mostUsers[0][x]['byUser'] + '.json?print=pretty';
+        await this.appService.getData(params).then(function (this, results) {
+          if (results.karma >= 10000) {
+            mostTitles += mostUsers[0][x]['title'] + ' ';
+          }
+        });
+      }
+
+      mostTitles.toLowerCase();
+      mostTitles.replace(/[^0-9a-z ]/g, '');
+      const finalResults = this.sortCounter(mostTitles);
+
+      console.log(finalResults);
+      return finalResults;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { response } from 'express';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  async getData(params) {
+    return lastValueFrom(
+      await this.httpService
+        .get(`https://hacker-news.firebaseio.com/v0/${params.HNLINK}`)
+        .pipe(map((response) => response.data)),
+    );
   }
 }
